c426f18455cf0391c2931ea10e15bb1153d482c3
Zack-Heisnberg <69718435+Zack-Heisnberg@users.noreply.github.com>
2021-08-24 09:46:55 +0100

    Create pos.js

---
+++ public/api/pos.js
@@ -0,0 +1,4 @@
+{
+"l": ["https://oneclick-pos1.cfapps.eu10.hana.ondemand.com","https://oneclick-pos1.herokuapp.com"],
+"h": ["https://oneclick-pos1.cfapps.eu10.hana.ondemand.com","https://oneclick-pos1.herokuapp.com"]
+}
